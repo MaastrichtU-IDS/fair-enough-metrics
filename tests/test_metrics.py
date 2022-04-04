a58138de79244e8171233a3cb69effc15e720bef
Vincent Emonet <vincent.emonet@gmail.com>
2022-04-04 12:52:04 +0200

    add option to tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,11 +1,11 @@
 from fastapi.testclient import TestClient
 from main import app
+import pytest
 
 # Test all Metrics tests using the URL and expected score 
 # defined with their test_test attribute
 
 endpoint = TestClient(app)
 
-
-def test_api():
-    app.run_tests(endpoint)
+def test_api(pytestconfig):
+    app.run_tests(endpoint, pytestconfig.getoption('metric'))
